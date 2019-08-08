9bbf594ebfb4ac7929a565fe61ef3c69c8bb1ea8
AndrewMarellspok <52172208+andrewmarellspok@users.noreply.github.com>
2019-08-08 14:54:11 -0500

    Update insert.sh

--- insert.sh
+++ insert.sh
@@ -1,3 +1,4 @@
+STARTTIME=$(date +%s)
 INPUT=Docgraph_30_Vertex.csv
 OLDIFS=$IFS
 IFS=,
@@ -6,5 +7,6 @@ while read id
 do
 	echo $id
 	curl -X GET localhost:9000/query/graph500/Insert_Vertex?id=id
-done < $INPUT
-IFS=$OLDIFS
+done 
+ENDTIME=$(date +%s)
+echo "It takes $($ENDTIME - $STARTTIME) seconds to complete this task..."
