210fa198f9a4bd2685a936e548235d96d4921ad9
AndrewMarellspok <52172208+andrewmarellspok@users.noreply.github.com>
2019-08-08 14:55:46 -0500

    Update insert.sh

--- insert.sh
+++ insert.sh
@@ -5,7 +5,7 @@ IFS=,
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 while read id 
 do
-	echo $id
+	echo "$id"
 	curl -X GET localhost:9000/query/graph500/Insert_Vertex?id=id
 done 
 ENDTIME=$(date +%s)
